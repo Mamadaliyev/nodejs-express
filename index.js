430bddde0d83d8569f1a4511e769e945aa586148
shohruh1999 <shohruh@3i.ai>
2021-01-31 18:37:37 +0500

    express router example

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
+
+const dishRouter = require("./routes/dishRouter");
+app.use("/dishes", dishRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);
