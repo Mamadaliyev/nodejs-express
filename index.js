fc91e4623d266a8f7efc76c076ea6d8d7937035d
shohruh1999 <shohruh@3i.ai>
2021-01-31 20:00:13 +0500

    added /leaders & /promotions

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);
